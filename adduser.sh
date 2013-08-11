cb33cc26a65e14a920ab559b776669a55db98325
Fernando Mercês <nandu88@gmail.com>
2013-08-11 03:36:38 -0300

    changed ftp readme filename to ftp-readme.txt

--- adduser.sh
+++ adduser.sh
@@ -4,7 +4,7 @@ source aleph.conf
 
 adduser --home "$ftp_incoming_dir"/$1 $1
 addgroup $1 $ftp_group
-ln "$ftp_incoming_dir"/readme.txt "$ftp_incoming_dir"/$1/readme.txt
+ln "$ftp_incoming_dir"/ftp-readme.txt "$ftp_incoming_dir"/$1/readme.txt
 mkdir "$ftp_incoming_dir"/$1/incoming
 chown root: "$ftp_incoming_dir"/$1
 chown -R $1: "$ftp_incoming_dir"/$1/incoming
