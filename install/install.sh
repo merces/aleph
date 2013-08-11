8135de14695b0068a924a88146f63785cff98e1e
aleph <aleph@aleph>
2013-08-11 04:28:43 -0300

    install script improvments

--- install/install.sh
+++ install/install.sh
@@ -6,35 +6,48 @@
 # passwd aleph
 # addgroup aleph sudo
 
-# some security
+ask() {
+	echo
+	read -p "$1... Continue [Y/n]? " opt
+	[ "$opt" = "n" ] && exit	
+} 
+
+echo -e '\naleph installation script\n'
+ask 'Set up hardening'
 bash firewall.sh
 # hardening + ipv6 disabling
 cp hardening.conf /etc/sysctl.d
 sysctl -p /etc/sysctl.d/hardening.conf
 
 
+ask 'Install packages using the internet'
 # basic packages
 apt-get update && apt-get upgrade -y
 apt-get install vsftpd ssh apache2 libapache2-mod-php5 git file zip unzip rar \
-unrar bzip2 gzip curl exim4 ntpdate gcc make rcconf vim
+unrar bzip2 gzip curl exim4 ntpdate gcc make rcconf vim libjansson-dev \
+libssl-dev libpcre3-dev
 
+ask 'Set timezone and current time'
 # setting time
 #dpkg-reconfigure tzdata
 ntpdate ntp.cais.rnp.br
 
-#pev
-wget https://github.com/merces/libpe/archive/master.zip
-unzip master.zip
+ask 'Download and build libpe'
+wget https://github.com/merces/libpe/archive/master.zip -O libpe.zip
+unzip libpe.zip 
 cd libpe-master
-make && make install
-apt-get install libssl-dev libpcre3-dev
-wget https://github.com/merces/pev/archive/master.zip
-unzip master.zip
+make
+make install
+cd ..
+
+ask 'Download and build pev'
+wget https://github.com/merces/pev/archive/master.zip -O pev.zip
+unzip pev.zip 
 cd pev-master
+mv ../libpe-master/* lib/libpe/
 make && make install
 
-#jshon
-apt-get install libjansson-dev
+ask 'Download and install json'
 wget http://kmkeen.com/jshon/jshon.tar.gz
 tar xf jshon.tar.gz
 cd jshon*
@@ -43,18 +56,18 @@ gzip -c -9 jshon.1 > jshon.1.gz
 install jshon /usr/local/bin/jshon
 install jshon.1.gz /usr/share/man/man1/jshon.1.gz
 
-# vim
+ask 'Configure vim'
 echo -e "syntax on\nset nu\nset tabstop=3" > /home/aleph/.vimrc
 cp /home/aleph/.vimrc /root
 chown aleph: /home/aleph/.vimrc
 
-# ssh
+ask 'Configure OpenSSH Server'
 addgroup aleph-users
 sed -i 's/^PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
 echo -e "\nDenyGroups aleph-users" >> /etc/ssh/sshd_config
 service ssh reload
 
-# apache httpd
+ask 'Configure Apache httpd'
 a2dismodule autoindex
 a2dissite default default-ssl
 a2enmod ssl
@@ -62,9 +75,9 @@ cp aleph-ssl /etc/apache2/sites-available
 a2ensite aleph-ssl
 sed -i 's/^NameVirtualHost \*:80/#NameVirtualHost \*:80/' apache2.conf
 sed -i 's/^Listen 80/#Listen 80/' /etc/apache2/ports.conf
-service apache restart
+service apache2 restart
 
-# vsftp
+ask 'Configure vsftpd'
 sed -i 's/^anonymous_enable=YES/anonymous_enable=NO/' /etc/vsftpd.conf
 sed -i 's/^#local_enable=YES/local_enable=YES/' /etc/vsftpd.conf
 sed -i 's/^#write_enable=YES/write_enable=YES/' /etc/vsftpd.conf
@@ -79,7 +92,3 @@ mv aleph-master/* /home/aleph/
 chown -R aleph: /home/aleph
 # ftp readme.txt
 cp ftp-readme.txt /home/incoming
-
-# cleaning
-cd ~
-rm -r .bash_history *
