f16dd00ce07aeb58bf5559d082247fae617285d5
Pr0teus <felipeaesposito@gmail.com>
2014-08-20 02:02:23 -0300

    VirusTotal Plugin

---
+++ aleph/plugins/virustotal.py
@@ -0,0 +1,28 @@
+# -*- coding: utf8 -*-
+
+from aleph.base import PluginBase
+from aleph.settings import VIRUSTOTAL_KEY
+import virustotal
+
+class VirustotalPlugin(PluginBase):
+
+    name = 'Virustotal'
+   
+    def process(self, sample):
+	vt = virustotal.VirusTotal(VIRUSTOTAL_KEY,0)
+        report = vt.get(sample.hashes['sha256'])	
+	if report is None:
+           report = vt.get(sample.path)
+           report.join()
+        if report.done ==True:
+	   return {
+		'virustotal': report.id,
+		'positives': report.positives,
+		'total': report.total,
+		'antivirus': str(dict(report)),
+		}
+    
+
+def setup(queue):
+    plugin = VirustotalPlugin(queue)
+    return plugin
