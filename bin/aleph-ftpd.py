f974d496b7a61534b299ad84a07d99c4f93d2655
Fernando Mercês <fernando@mentebinaria.com.br>
2014-12-20 19:38:35 -0200

    add integrated FTP server

---
+++ bin/aleph-ftpd.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import os, sys
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+# Fix path for importing modules
+CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
+PACKAGE_DIR = os.path.abspath(os.path.join(CURRENT_DIR, os.pardir))
+sys.path.append(PACKAGE_DIR)
+
+from aleph.webui.models import User
+from aleph.webui.utils import hash_password
+from aleph.settings import SAMPLE_SUBMIT_FOLDER
+
+# Overriding validate_authentication() to support to Aleph's auth method
+class DummyHashAuthorizer(DummyAuthorizer):
+
+    def validate_authentication(self, username, password, handler):
+        hash = hash_password(username, password)
+        return self.user_table[username]['pwd'] == hash
+
+authorizer = DummyHashAuthorizer()
+
+# Add active premium users to the FTP Server
+for user in User.query.filter(User.active, User.account_type < 2):
+    authorizer.add_user(user.login, user.password, SAMPLE_SUBMIT_FOLDER, perm='elradfmw')
+
+handler = FTPHandler
+#handler.banner = "pyftpdlib based ftpd ready."
+handler.authorizer = authorizer
+address = ('', 2121)
+server = FTPServer(address, handler)
+
+# set a limit for connections
+server.max_cons = 256
+server.max_cons_per_ip = 5
+
+# start ftp server
+server.serve_forever()
