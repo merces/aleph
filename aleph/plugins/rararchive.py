610867b3f4c0399becfc11fe2c6ccb5b0f3b3e71
Fernando Mercês <fernando@mentebinaria.com.br>
2014-12-21 12:43:20 -0200

    added RAR extraction plugin

---
+++ aleph/plugins/rararchive.py
@@ -0,0 +1,71 @@
+from rarfile import RarFile
+from tempfile import mkdtemp
+from aleph.base import PluginBase
+import shutil, os, ntpath
+
+from aleph.settings import SAMPLE_TEMP_DIR, SAMPLE_MIN_FILESIZE
+
+class RarArchivePlugin(PluginBase):
+
+    name = 'rararchive'
+    default_options = { 'passwords': [ 'infected', 'evil', 'virus', 'malicious' ], 'enabled': True }
+    mimetypes = ['application/x-rar']
+    
+    def extract_file(self, path, dest, password=None):
+
+        nl = []
+
+        with RarFile(path, 'r') as rarf:
+            if password:
+                rarf.setpassword(password)
+            rarf.extractall(dest)
+            nl = rarf.namelist()
+
+        return nl
+
+    def process(self):
+
+        temp_dir = mkdtemp(dir=SAMPLE_TEMP_DIR)
+
+        self.options['passwords'].insert(0, '') # Append blank password
+        current_password = None
+        rar_contents = []
+
+        for password in set(self.options['passwords']):
+            current_password = password
+            try:
+                self.logger.debug("Uncompressing file %s with password '%s'" % (self.sample.path, password))
+                rar_contents = self.extract_file(self.sample.path, temp_dir, password)
+
+                for fname in rar_contents:
+                    fpath = os.path.join(temp_dir, fname)
+                    if os.path.isfile(fpath) and os.stat(fpath).st_size >= SAMPLE_MIN_FILESIZE:
+                        head, tail = ntpath.split(fpath)
+                        self.create_sample(fpath, tail)
+                shutil.rmtree(temp_dir)
+                break # Stop bruting
+            except RuntimeError:
+                continue # Invalid password
+
+        ret = {}
+
+        # Add general tags
+        self.sample.add_tag('archive')
+        self.sample.add_tag('rar')
+
+        if len(rar_contents) == 0:
+            self.logger.error('Unable to uncompress %s. Invalid password or corrupted file' % self.sample.path)
+            self.sample.add_tag('corrupt')
+            return ret
+
+        ret['contents'] = rar_contents
+
+        if len(current_password) > 0:
+            self.sample.add_tag('password-protected')
+            ret['password'] = current_password
+
+        return ret
+
+def setup(queue):
+    plugin = RarArchivePlugin(queue)
+    return plugin
