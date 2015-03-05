2c6be58c453bedf8bfa1cb7faf24cbdf0de97633
Neriberto C.Prado <neriberto@gmail.com>
2015-03-05 08:40:10 -0300

    Using elastisearch index variable instead of strings, creating a variable for doc type

--- aleph/datastore.py
+++ aleph/datastore.py
@@ -5,10 +5,12 @@ import logging
 from aleph.utils import dict_merge
 from aleph.settings import ELASTICSEARCH_URI, ELASTICSEARCH_INDEX, ELASTICSEARCH_TRACE, LOGGING
 
+
 class DataStore(object):
 
     es = None
     tracer = None
+    DOC_TYPE = "sample"
 
     def __init__(self):
 
@@ -22,22 +24,21 @@ class DataStore(object):
             self.tracer.addHandler(logging.NullHandler())
 
     def update(self, doc_id, partial_body):
-        self.es.update(index=ELASTICSEARCH_INDEX, id=doc_id, doc_type='sample', body={'doc': partial_body })
+        self.es.update(index=ELASTICSEARCH_INDEX, id=doc_id, doc_type=self.DOC_TYPE, body={'doc': partial_body })
 
     def setup(self):
         self.es.indices.create(index=ELASTICSEARCH_INDEX, ignore=400) # Ignore already exists
 
     def count(self, q=None):
-
         if q:
-            result = self.es.count(index=ELASTICSEARCH_INDEX, doc_type='sample', q=q)
+            result = self.es.count(index=ELASTICSEARCH_INDEX, doc_type=self.DOC_TYPE, q=q)
         else:
-            result = self.es.count(index=ELASTICSEARCH_INDEX, doc_type='sample')
+            result = self.es.count(index=ELASTICSEARCH_INDEX, doc_type=self.DOC_TYPE)
         return result['count']
 
     def all(self, size=10, start=0):
         try:
-            result = self.es.search(index=ELASTICSEARCH_INDEX, doc_type='sample', body={
+            result = self.es.search(index=ELASTICSEARCH_INDEX, doc_type=self.DOC_TYPE, body={
                 'query': {
                     'match_all': {},
                 },
@@ -67,7 +68,7 @@ class DataStore(object):
                     },
                 }
             }
-            result = self.es.search(index=ELASTICSEARCH_INDEX, doc_type='sample', q=query, from_=start, size=size, body=body)
+            result = self.es.search(index=ELASTICSEARCH_INDEX, doc_type=self.DOC_TYPE, q=query, from_=start, size=size, body=body)
         except NotFoundError:
             pass
         except Exception:
@@ -80,7 +81,7 @@ class DataStore(object):
         result = []
 
         try:
-            result = self.es.search(index=ELASTICSEARCH_INDEX, doc_type='sample', body={'query': {'term': query } })
+            result = self.es.search(index=ELASTICSEARCH_INDEX, doc_type=self.DOC_TYPE, body={'query': {'term': query } })
         except NotFoundError:
             pass
         except Exception:
@@ -89,14 +90,16 @@ class DataStore(object):
         return result
 
     def save(self, doc_data, doc_id):
-        return self.merge_document('samples', 'sample', doc_data, doc_id)
+        try:
+            return self.merge_document(ELASTICSEARCH_INDEX, self.DOC_TYPE, doc_data, doc_id)
+        except Exception as ex:
+            self.logger.warn(str(ex));
+            return None
 
     def get(self, doc_id):
-
-        return self.es.get(index='samples', doc_type='sample', id=doc_id)['_source']
+        return self.es.get(index=ELASTICSEARCH_INDEX, doc_type=self.DOC_TYPE, id=doc_id)['_source']
 
     def merge_document(self, index, doc_type, doc_data, doc_id):
-
         try:
             self.es.indices.refresh(index)
         except Exception as e:
