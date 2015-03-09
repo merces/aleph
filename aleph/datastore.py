2133e5c7375db37494688e60a6b42b91bd7e3456
Neriberto C.Prado <neriberto@gmail.com>
2015-03-09 09:27:32 -0300

    Using elasticsearch index variable instead of strings, creating a variable for doc type

--- aleph/datastore.py
+++ aleph/datastore.py
@@ -3,7 +3,12 @@ from elasticsearch import Elasticsearch, NotFoundError
 import logging
 
 from aleph.utils import dict_merge
-from aleph.settings import ELASTICSEARCH_URI, ELASTICSEARCH_INDEX, ELASTICSEARCH_TRACE, LOGGING
+from aleph.settings import ELASTICSEARCH_URI
+from aleph.settings import ELASTICSEARCH_INDEX
+from aleph.settings import ELASTICSEARCH_DOCTYPE
+from aleph.settings import ELASTICSEARCH_TRACE
+from aleph.settings import LOGGING
+
 
 class DataStore(object):
 
@@ -22,22 +27,29 @@ class DataStore(object):
             self.tracer.addHandler(logging.NullHandler())
 
     def update(self, doc_id, partial_body):
-        self.es.update(index=ELASTICSEARCH_INDEX, id=doc_id, doc_type='sample', body={'doc': partial_body })
+        self.es.update(index=ELASTICSEARCH_INDEX,
+                       id=doc_id,
+                       doc_type=ELASTICSEARCH_DOCTYPE,
+                       body={'doc': partial_body})
 
     def setup(self):
-        self.es.indices.create(index=ELASTICSEARCH_INDEX, ignore=400) # Ignore already exists
+        # Ignore already exists
+        self.es.indices.create(index=ELASTICSEARCH_INDEX, ignore=400)
 
     def count(self, q=None):
 
         if q:
-            result = self.es.count(index=ELASTICSEARCH_INDEX, doc_type='sample', q=q)
+            result = self.es.count(index=ELASTICSEARCH_INDEX,
+                                   doc_type=ELASTICSEARCH_DOCTYPE,
+                                   q=q)
         else:
-            result = self.es.count(index=ELASTICSEARCH_INDEX, doc_type='sample')
+            result = self.es.count(index=ELASTICSEARCH_INDEX,
+                                   doc_type=ELASTICSEARCH_DOCTYPE)
         return result['count']
 
     def all(self, size=10, start=0):
         try:
-            result = self.es.search(index=ELASTICSEARCH_INDEX, doc_type='sample', body={
+            _body = {
                 'query': {
                     'match_all': {},
                 },
@@ -48,14 +60,16 @@ class DataStore(object):
                         'order': 'desc'
                     },
                 }
-                })
+            }
+            result = self.es.search(index=ELASTICSEARCH_INDEX,
+                                    doc_type=ELASTICSEARCH_DOCTYPE,
+                                    body=_body)
         except NotFoundError:
             pass
         except Exception:
             raise
 
         return result
-
 
     def lucene_search(self, query, start=0, size=15):
 
@@ -67,7 +81,12 @@ class DataStore(object):
                     },
                 }
             }
-            result = self.es.search(index=ELASTICSEARCH_INDEX, doc_type='sample', q=query, from_=start, size=size, body=body)
+            result = self.es.search(index=ELASTICSEARCH_INDEX,
+                                    doc_type=ELASTICSEARCH_DOCTYPE,
+                                    q=query,
+                                    from_=start,
+                                    size=size,
+                                    body=body)
         except NotFoundError:
             pass
         except Exception:
@@ -80,7 +99,9 @@ class DataStore(object):
         result = []
 
         try:
-            result = self.es.search(index=ELASTICSEARCH_INDEX, doc_type='sample', body={'query': {'term': query } })
+            result = self.es.search(index=ELASTICSEARCH_INDEX,
+                                    doc_type=ELASTICSEARCH_DOCTYPE,
+                                    body={'query': {'term': query}})
         except NotFoundError:
             pass
         except Exception:
@@ -89,11 +110,19 @@ class DataStore(object):
         return result
 
     def save(self, doc_data, doc_id):
-        return self.merge_document('samples', 'sample', doc_data, doc_id)
+        try:
+            return self.merge_document(ELASTICSEARCH_INDEX,
+                                       ELASTICSEARCH_DOCTYPE,
+                                       doc_data,
+                                       doc_id)
+        except Exception as ex:
+            self.logger.warn(str(ex))
+            return None
 
     def get(self, doc_id):
-
-        return self.es.get(index='samples', doc_type='sample', id=doc_id)['_source']
+        return self.es.get(index=ELASTICSEARCH_INDEX,
+                           doc_type=ELASTICSEARCH_DOCTYPE,
+                           id=doc_id)['_source']
 
     def merge_document(self, index, doc_type, doc_data, doc_id):
 
@@ -106,22 +135,29 @@ class DataStore(object):
 
         # Try to get current data if available
         try:
-            original_document = self.es.get(index=index, doc_type=doc_type, id=doc_id)
-            if 'hits' in original_document and original_document['hits']['total'] != 0:
+            original_document = self.es.get(index=index,
+                                            doc_type=doc_type,
+                                            id=doc_id)
+            total = original_document['hits']['total']
+            if 'hits' in original_document and total != 0:
                 original_document = original_document['_source']
             else:
                 original_document = {}
         except NotFoundError as e:
-            pass # not found, proceed
+            # not found, proceed
+            pass
         except Exception as e:
-            raise e 
+            raise e
 
-        if len(original_document) == 0 :
+        if len(original_document) == 0:
             return self.es.index(index, doc_type, doc_data, id=doc_id)
 
         # Merge and index
         merged_document = dict_merge(original_document, doc_data)
 
-        return self.es.index(index=index, doc_type=doc_type, body=merged_document, id=doc_id)
+        return self.es.index(index=index,
+                             doc_type=doc_type,
+                             body=merged_document,
+                             id=doc_id)
 
 es = DataStore()
