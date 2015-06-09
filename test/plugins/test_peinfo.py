bc54ae105bc1ab11e93b258a33e76177c116d029
Alvaro Rios <alvaro@nbptrust.com.br>
2015-06-09 16:19:40 -0300

    fix indentation

--- test/plugins/test_peinfo.py
+++ test/plugins/test_peinfo.py
@@ -13,9 +13,9 @@ class PEInfoPluginTestCase(unittest.TestCase):
 
     def test_it_get_pe_data(self):
         response = {'compilation_date': '2009-07-14 01:12:13', 'aslr': True, 'dep': True, 'architechture': '32-bit', 'number_sections': 1, 'compilation_timestamp': 1247533933, 'sections': [{'virtual_size': '0x538', 'name': '.rsrc', 'raw_size': 1536, 'address': '0x1000'}], 'seh': True}
-    	queue = Mock()
-    	sample = Mock()
-    	sample.path = 'test/data/wmerror.dll'
+        queue = Mock()
+        sample = Mock()
+        sample.path = 'test/data/wmerror.dll'
         pe = PEInfoPlugin(queue)
         pe.set_sample(sample)
         self.assertDictEqual(response, pe.get_pe_data())
