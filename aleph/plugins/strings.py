4dd883efccb1de0ceaa2456764e6c0e54e3ea00d
Jan Seidl <jseidl@wroot.org>
2015-11-21 17:04:08 -0200

    Added @turicas implementation of the decoder for strings plugin on issue #28

--- aleph/plugins/strings.py
+++ aleph/plugins/strings.py
@@ -7,7 +7,7 @@ import re
 class StringsPlugin(PluginBase):
     """Try to Extract Strings from the sample (URL/filesnames/e-mails) just like the unix command strings"""
     name = 'strings'
-    default_options = { 'enabled': True }
+    default_options = { 'enabled': True, 'mode': 'printable', 'decoder_codecs': ['utf-8','ascii','iso-8859-1'] }
     mimetypes_except = MIMETYPES_ARCHIVE + ['text/url']
 
     url_regex = ur'(?i)\b((?:http[s]?:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s!()\[\]{};:\'".,<>?]))'
@@ -18,7 +18,7 @@ class StringsPlugin(PluginBase):
     pattern_filename = re.compile(filename_regex)
     pattern_emailaddr = re.compile(emailaddr_regex)
 
-    def extract_strings(self, data):
+    def extract_printable_strings(self, data):
         result = ""
         for c in data:
             if c in string.printable:
@@ -28,11 +28,17 @@ class StringsPlugin(PluginBase):
                 yield result
             result = ""
 
+    def extract_strings_decoded(self, data):
+        return = set(data.decode(codec, 'ignore') for codec in self.options['decoder_codecs'])
+
     def process(self):
 
         with open(self.sample.path) as f:
 
-            string_list = self.extract_strings(f.read())
+            if self.options['mode'] == 'printable':
+                string_list = self.extract_strings_decoded(f.read())
+            else:
+                string_list = self.extract_strings_printable(f.read())
 
             emailaddr_strings = []
             url_strings = []
